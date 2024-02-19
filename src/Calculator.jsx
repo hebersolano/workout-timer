d65c8ed3361514a7129c1a447a164e5d6f64d2d9
Heber Solano <hebersolano1@gmail.com>
2024-02-18 22:10:32 -0600

    reducer state

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,15 +1,59 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 import ToggleSounds from "./ToggleSounds";
 
-function Calculator({ workouts }) {
-  const [allowSound, setAllowSound] = useState(true);
-  const [number, setNumber] = useState(workouts.at(0).numExercises);
-  const [sets, setSets] = useState(3);
-  const [speed, setSpeed] = useState(90);
-  const [durationBreak, setDurationBreak] = useState(5);
+function reducer(state, action) {
+  const { type, payload } = action;
+  let { number, sets, speed, durationBreak, duration } = state;
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  switch (type) {
+    case "initDuration": {
+      duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+      return { ...state, duration };
+    }
+    case "setNumber": {
+      duration = (payload * sets * speed) / 60 + (sets - 1) * durationBreak;
+      return { ...state, number: payload, duration };
+    }
+    case "setSets": {
+      duration = (number * payload * speed) / 60 + (sets - 1) * durationBreak;
+      return { ...state, sets: payload, duration };
+    }
+    case "setSpeed": {
+      duration = (number * sets * payload) / 60 + (sets - 1) * durationBreak;
+      return { ...state, speed: payload, duration };
+    }
+    case "setBreak": {
+      duration = (number * sets * speed) / 60 + (sets - 1) * payload;
+      return { ...state, durationBreak: payload, duration };
+    }
+    case "duration+": {
+      return { ...state, duration: ++duration };
+    }
+    case "duration-": {
+      return { ...state, duration: --duration };
+    }
+  }
+}
+
+function Calculator({ workouts }) {
+  const [state, dispatch] = useReducer(reducer, {
+    number: workouts.at(0).numExercises,
+    sets: 3,
+    speed: 90,
+    durationBreak: 5,
+    duration: (workouts.at(0).numExercises * 3 * 90) / 60 + (3 - 1) * 5, //decided to calculate the duration in the initState
+  });
+
+  const { number, sets, speed, durationBreak, duration } = state;
+  const [allowSound, setAllowSound] = useState(true);
+  // const [number, setNumber] = useState(workouts.at(0).numExercises);
+  // const [sets, setSets] = useState(3);
+  // const [speed, setSpeed] = useState(90);
+  // const [durationBreak, setDurationBreak] = useState(5);
+  // const [duration, setDuration] = useState(0);
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
@@ -19,13 +63,21 @@ function Calculator({ workouts }) {
     sound.play();
   };
 
+  // CALC INIT DURATION STATE if it is not calculate in the initState
+  // useEffect(function calcDurationInit() {
+  //   dispatch({ type: "initDuration" });
+  // }, []);
+
   return (
     <>
       <ToggleSounds allowSound={allowSound} setAllowSound={setAllowSound} />
       <form>
         <div>
           <label>Type of workout</label>
-          <select value={number} onChange={(e) => setNumber(+e.target.value)}>
+          <select
+            value={number}
+            onChange={(e) => dispatch({ type: "setNumber", payload: +e.target.value })}
+          >
             {workouts.map((workout) => (
               <option value={workout.numExercises} key={workout.name}>
                 {workout.name} ({workout.numExercises} exercises)
@@ -41,7 +93,7 @@ function Calculator({ workouts }) {
             min="1"
             max="5"
             value={sets}
-            onChange={(e) => setSets(e.target.value)}
+            onChange={(e) => dispatch({ type: "setSets", payload: +e.target.value })}
           />
           <span>{sets}</span>
         </div>
@@ -54,7 +106,7 @@ function Calculator({ workouts }) {
             max="180"
             step="30"
             value={speed}
-            onChange={(e) => setSpeed(e.target.value)}
+            onChange={(e) => dispatch({ type: "setSpeed", payload: +e.target.value })}
           />
           <span>{speed} sec/exercise</span>
         </div>
@@ -66,7 +118,7 @@ function Calculator({ workouts }) {
             min="1"
             max="10"
             value={durationBreak}
-            onChange={(e) => setDurationBreak(e.target.value)}
+            onChange={(e) => dispatch({ type: "setBreak", payload: +e.target.value })}
           />
           <span>{durationBreak} minutes/break</span>
         </div>
@@ -74,6 +126,7 @@ function Calculator({ workouts }) {
       <section>
         <button
           onClick={() => {
+            dispatch({ type: "duration-" });
             playSound();
           }}
         >
@@ -86,6 +139,7 @@ function Calculator({ workouts }) {
         </p>
         <button
           onClick={() => {
+            dispatch({ type: "duration+" });
             playSound();
           }}
         >
